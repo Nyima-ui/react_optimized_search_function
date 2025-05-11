9acc9d39c04b6d890d5abd0b8ea4a7403b19b8e3
Nyima-ui <ntenzin492@gmail.com>
2025-05-11 10:02:55 +0530

    implemented infinie scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchText, setSearchText] = useState("");
+  const [books, setBooks] = useState([]);
+  const [displayedBooks, setDisplayedBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [debouncedTerm, setDebouncedTerm] = useState("");
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(searchText);
+    }, 300);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [searchText]);
+
+  useEffect(() => {
+    const fetchBooks = async (title) => {
+      if (!title) {
+        setBooks([]);
+        setDisplayedBooks([]);
+        return;
+      }
+      setLoading(true);
+      setDisplayedBooks([]);
+      try {
+        const response = await fetch(
+          `https://openlibrary.org/search.json?title=${title}`
+        );
+        if (!response.ok)
+          throw new Error(`HTTP error! status: ${response.status}`);
+
+        const result = await response.json();
+        setBooks(result.docs || []);
+      } catch (error) {
+        console.log("Fetching books failed", error);
+        setBooks([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchBooks(debouncedTerm);
+  }, [debouncedTerm]);
+
+  useEffect(() => {
+    setDisplayedBooks(books.slice(0, 20));
+  }, [books]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isNearBottom =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight;
+
+      const hasMoreBooks = displayedBooks.length < books.length;
+
+      if (
+        isNearBottom &&
+        !loading &&
+        hasMoreBooks &&
+        displayedBooks.length > 0
+      ) {
+        console.log("Condition met: Loading next batch of books!");
+
+        const nextBatchStartIndex = displayedBooks.length;
+
+        const nextBatchEndIndex = Math.min(
+          books.length,
+          nextBatchStartIndex + 20
+        );
+
+        const nextBatch = books.slice(nextBatchStartIndex, nextBatchEndIndex);
+
+        setDisplayedBooks((prevDisplayedBooks) => [
+          ...prevDisplayedBooks,
+          ...nextBatch,
+        ]);
+      }
+
+      //  if(isNearBottom && !loading &&)
+      console.log(isNearBottom);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [loading, books, displayedBooks]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="text"
+        className="input_field"
+        placeholder="Search for books"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      {loading && <h1 className="loading_text">Loading...</h1>}
+
+      <section className="search_results">
+        {displayedBooks?.map((book, i) => (
+          <p key={i} className="book_titles">
+            {book.title}
+          </p>
+        ))}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
